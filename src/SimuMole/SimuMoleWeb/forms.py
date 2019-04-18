77c59c206721ceaead0de7612fcca1e77ff645a7
IlanitSmul <ilanitsmul@campus.technion.ac.il>
2019-04-18 22:20:22 +0200

    issue #69: add an option to access data from previous steps/forms

--- src/SimuMole/SimuMoleWeb/forms.py
+++ src/SimuMole/SimuMoleWeb/forms.py
@@ -1,7 +1,8 @@
 from django import forms
 
+
 # todo 1: replace CharField with DecimalField, when needed
-# todo 2: add validation
+# todo 2: add validation tests (examples below)
 
 class SimulationForm1_LoadPdb(forms.Form):
     first_pdb = forms.CharField(
@@ -12,6 +13,10 @@ class SimulationForm1_LoadPdb(forms.Form):
         required=True,
         label='Enter your second pdb ID',
         max_length=10)
+
+    def clean(self):
+        cleaned_data = super(SimulationForm1_LoadPdb, self).clean()
+        data = {**self.initial, **cleaned_data}  # self.initial->from previous steps, cleaned_data->from current step
 
 
 class SimulationForm2_DetermineRelativePosition(forms.Form):
@@ -40,6 +45,10 @@ class SimulationForm2_DetermineRelativePosition(forms.Form):
         label='Enter delta z of second pdb',
         max_length=10)
 
+    def clean(self):
+        cleaned_data = super(SimulationForm2_DetermineRelativePosition, self).clean()
+        data = {**self.initial, **cleaned_data}  # self.initial->from previous steps, cleaned_data->from current step
+
 
 class SimulationForm3_SimulationParameters(forms.Form):
     temperature = forms.CharField(
@@ -47,3 +56,6 @@ class SimulationForm3_SimulationParameters(forms.Form):
         label='Enter temperature',
         max_length=10)
 
+    def clean(self):
+        cleaned_data = super(SimulationForm3_SimulationParameters, self).clean()
+        data = {**self.initial, **cleaned_data}  # self.initial->from previous steps, cleaned_data->from current step
--- src/SimuMole/SimuMoleWeb/views.py
+++ src/SimuMole/SimuMoleWeb/views.py
@@ -1,11 +1,15 @@
-from django.shortcuts import render, render_to_response
+from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import Http404
 from django.template import loader
 
+from formtools.wizard.views import CookieWizardView  # todo: check if need to replace with "SessionWizardView"
+
 from SimuMoleScripts.simulation_main_script import Simulation
 
-from formtools.wizard.views import CookieWizardView  # todo: check if need to replace with "SessionWizardView"
+from SimuMoleWeb.forms import SimulationForm1_LoadPdb, \
+    SimulationForm2_DetermineRelativePosition, \
+    SimulationForm3_SimulationParameters
 
 
 ################################
@@ -24,6 +28,7 @@ def home(request):
 class SimulationWizard(CookieWizardView):
     template_name = 'create_simulation.html'
 
+    # override "done": this function is called when the form is submitted
     def done(self, form_list, **kwargs):
         form_data = [form.cleaned_data for form in form_list]
 
@@ -48,3 +53,29 @@ class SimulationWizard(CookieWizardView):
         return render(self.request, 'create_simulation_result.html', {
             'form_data': form_data,
         })
+
+    # override "get_form_initial" for allow getting data from previous steps
+    def get_form_initial(self, step):
+        current_step = self.storage.current_step
+        initial_data = self.initial_dict.get(step, {})  # initial data of current step
+        prev_data = {}  # data of previous step
+
+        if current_step == '0':  # SimulationForm1_LoadPdb
+            prev_data = {}
+
+        if current_step == '1':  # SimulationForm2_DetermineRelativePosition
+            prev_data = self.storage.get_step_data('0')
+            prev_data = {'first_pdb': prev_data.get('0-first_pdb', ''), 'second_pdb': prev_data.get('0-second_pdb', '')}
+
+        if current_step == '2':  # SimulationForm3_SimulationParameters
+            prev_prev_data = self.storage.get_step_data('0')
+            prev_data = self.storage.get_step_data('1')
+            prev_data = {
+                'first_pdb': prev_prev_data.get('0-first_pdb', ''),
+                'second_pdb': prev_prev_data.get('0-second_pdb', ''),
+                'x1': prev_data.get('1-x1', ''), 'x2': prev_data.get('1-x2', ''),
+                'y1': prev_data.get('1-y1', ''), 'y2': prev_data.get('1-y2', ''),
+                'z1': prev_data.get('1-z1', ''), 'z2': prev_data.get('1-z2', ''), }
+
+        update_data = {**prev_data, **initial_data}
+        return self.initial_dict.get(step, update_data)
